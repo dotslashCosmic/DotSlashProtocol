bb69e8050e434fc2b35f01343a6a237d76917d62
/cosmic <91699202+dotslashCosmic@users.noreply.github.com>
2024-06-12 03:09:38 -0400

    Update dsp.py
    Args

--- dsp.py
+++ dsp.py
@@ -1,58 +1,60 @@
-#DotSlashProtocol - A TCP/IP Fork
+#DotSlashProtocol - A TCP/IPv4 Fork
 #Author: dotSlashCosmic
+#TODO Fix the encryption. commented code is encryption related. requires pycryptodome
 
-import socket, time, binascii, struct, os, logging, re
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import argparse, socket, time, struct, os, logging, time, re, getpass, zlib, uuid
+#from Crypto.Cipher import AES
+#from Crypto.Hash import SHA3_512
+#from Crypto.Util.Padding import pad, unpad
 
-class ECC():
-    def __init__(self):
-        self.private_key = ec.generate_private_key(ec.SECP256R1())
-        self.public_key = self.private_key.public_key()
+#class Encrypt:
+#    def __init__(self, password):
+#        self.password = password
+#        hash_object = SHA3_512.new(self.password.encode())
+#        self.hashed_password = hash_object.digest()[:32]
 
-    def encrypt(self, data):
-        shared_key = self.private_key.exchange(ec.ECDH(), self.public_key)
-        derived_key = HKDF(
-            algorithm=hashes.SHA512(),
-            length=64,
-            salt=None,
-            info=None,
-        ).derive(shared_key)
-        aes_key = derived_key[:32]
-        nonce = os.urandom(12)
-        aesgcm = AESGCM(aes_key)
-        ct = aesgcm.encrypt(nonce, data, None)
-        return (nonce, ct)
+#    def encrypt_data(self, data):
+#        if isinstance(data, str):
+#            data = data.encode()
+#        print('Data:',data)
+#        compressed_data = zlib.compress(data)
+#        cipher = AES.new(self.hashed_password, AES.MODE_CBC)
+#        ct_bytes = cipher.encrypt(pad(compressed_data, AES.block_size))
+#        iv = cipher.iv
+#        encrypted_data = bytes([(b + iv[0]) % 256 for b in ct_bytes])
+#        encrypted_data_with_iv = iv + encrypted_data
+#        print('Encrypted Data:', encrypted_data)
+#        print('IV:', iv)
+#        return encrypted_data_with_iv
 
-    def decrypt(self, nonce, ct):
-        shared_key = self.private_key.exchange(ec.ECDH(), self.public_key)
-        derived_key = HKDF(
-            algorithm=hashes.SHA512(),
-            length=64,
-            salt=None,
-            info=None,
-        ).derive(shared_key)
-        aes_key = derived_key[:32]
-        aesgcm = AESGCM(aes_key)
-        return aesgcm.decrypt(nonce, ct, None)
-        
+#    def decrypt_data(self, encrypted_data_with_iv):
+#        iv = encrypted_data_with_iv[:16]
+#        encrypted_data = encrypted_data_with_iv[16:]
+#        decrypted_data = bytes([(b - iv[0]) % 256 for b in encrypted_data])
+#        cipher = AES.new(self.hashed_password, AES.MODE_CBC, iv=iv)
+#        pt = unpad(cipher.decrypt(decrypted_data), AES.block_size)
+#        decompressed_pt = zlib.decompress(pt)
+#        print('Decrypted Data:', decompressed_pt.decode())
+#        return decompressed_pt.decode()
+
 class DSP:
-    def __init__(self, source_ip, dest_ip, source_port, dest_port, dest_mac, data):
-        self.source_ip = source_ip
-        self.dest_ip = dest_ip
-        self.source_port = source_port
-        self.dest_port = dest_port
-        self.dest_mac = dest_mac
+#    def __init__(self, source_ip, dest_ip, source_port, dest_port, source_mac, dest_mac, data, password):
+    def __init__(self, source_ip, dest_ip, source_port, dest_port, source_mac, dest_mac, data):
+        self.source_ip = srcip
+        self.dest_ip = dstip
+        self.source_port = srcport
+        self.source_mac = srcmac
+        self.dest_port = dstport
+        self.dest_mac = dstmac
         self.data = data
         self.frag_offset = 0
-    
+#        self.cipher = Encrypt(password)
+
     def dataint(self, t):
-        b = b''.join(part for part in t)
+        b = bytes(t)
         s = b.decode('utf-8', 'ignore')
         return len(s)
-        
+
     def hexify(self, input):
         if isinstance(input, str):
             return bytes(input, 'utf-8')
@@ -60,13 +62,14 @@ class DSP:
             return input.to_bytes((input.bit_length() + 7) // 8, 'big')
 
     def total_length(self):
-        length = self.dataint(self.encrypted_data)
+#        length = self.dataint(self.encrypted_data)
+        length = self.dataint(self.data.encode())
         total_length = int(length) + 36
         if total_length < 256:
             return b'\x00' + total_length.to_bytes(1, 'big')
         else:
             return total_length.to_bytes(2, 'big')
-    
+
     def dec_to_hex(self, d):
         return '\\x' + format(d, '02x')
 
@@ -75,32 +78,33 @@ class DSP:
             return ''.join('\\x'+hex(byte)[2:] for byte in s.to_bytes((s.bit_length() + 7) // 8, 'big'))
         else:
             return ''.join('\\x'+hex(byte)[2:] for byte in s.encode())
-            
+
     def str_to_bytes(self, s):
         return bytes.fromhex(s.replace('\\x', ''))
-        
+
     def data_to_bytes(self, d):
         return bytes(int(d[i:i+2], 16) for i in range(2, len(d), 4))
-        
+
     def tuple_to_bytes(self, t):
+        print(f"pre bytes tuple: {t}")
         return b''.join(part for part in t) 
-        
+
     def port_to_hex(self, port):
-        return '\\x' + '\\x'.join([hex(port)[2:].zfill(4)[i:i+2] for i in range(0, 4, 2)])
+        return '\\x' + '\\x'.join([hex(int(port))[2:].zfill(4)[i:i+2] for i in range(0, 4, 2)])
 
     def format_hex(self, hexf):
         return b''.join(rb'\x'+hexf[i:i+2] for i in range(0, len(hexf), 2))
-        
+
     def ip_to_bytes(self, ip):
         print(ip)
         parts = ip.split('.')
         hex_parts = ['\\x{:02x}'.format(int(part)) for part in parts]
         return ''.join(hex_parts)
-    
+
     def final_cs(self, c1, c2):
         final = c1 + c2
         return self.cs(final)
-        
+
     def cs(self, header):
         pos = len(header)
         if pos & 1:
@@ -116,7 +120,7 @@ class DSP:
         result = (~sum) & 0xffff
         hexresult = self.hexify(result)
         return hexresult
-        
+
     def cluster(self):
         cluster_id = os.urandom(2)
         hex_cluster_id = cluster_id.hex()
@@ -124,7 +128,7 @@ class DSP:
         hex_cluster_id = 'cc' + hex_cluster_id
         full_id = self.str_to_bytes(hex_cluster_id)
         return full_id
-        
+
     def fragmentation(self, data_bytes):
         max_data_size = (255**2) - 36
         while data_bytes:
@@ -136,16 +140,16 @@ class DSP:
                 yield (data_bytes, self.frag_offset)
                 data_bytes = b''
         return fragments
-                
+
     def log(self, packet):
         current_time = time.strftime("%Y%m%d-%H%M%S")
         logging.basicConfig(filename=f'packet_{current_time}.txt', level=logging.INFO, format='DotSlashProtocol:%(message)s')
         logging.info(packet)
 
     def dsp(self):
-        ecc = ECC()
-        self.encrypted_data = ecc.encrypt(self.data.encode())
-#TODO impliment fragmentation/large packet
+#        e = Encrypt(password)
+#        self.encrypted_data = e.encrypt_data(self.data.encode())
+        e = self.data.encode()
         fragments = self.fragmentation(self.data.encode())
         frag_offset = self.frag_offset
         header1 = b'\x45\x18' + self.total_length()# Version, IHL | Total Length of Packet
@@ -168,100 +172,89 @@ class DSP:
         print(header7, ' Reserved, Protocol | Header Checksum')
         header8 = b'\x21\xb2' + self.str_to_bytes(self.port_to_hex(len(self.data.encode())))# Data Offset, Reserved | Data Size
         print(header8, ' Data Offset, Reserved | Data Size')
-        header9 = self.tuple_to_bytes(self.encrypted_data)# Data, max of 255^2-36 bytes per fragment
+        header9 = self.data.encode()# Data, max of 255^2-36 bytes per fragment
         print(header9, ' Data')
         dataheader = header8 + header9
         header10 = self.cs(dataheader) + self.final_cs(self.cs(mainheader), self.cs(dataheader))# Data Checksum | Final Checksum
         packet = mainheader + dataheader + header10
         print(header10, ' Data Checksum | Final Checksum')
-        print('\nPACKET:', packet, '\n\nDATA PLAINTEXT:', self.data)
+        print('\nDATA PLAINTEXT:', self.data)
         self.log(packet)
         eths = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
         interface = 'eth0'
-        src_mac = 'c0:53:1c:c0:53:1c'
         eth_type = 0x0800
         src_mac_bytes = bytes.fromhex(src_mac.replace(':', ''))
         dst_mac_bytes = bytes.fromhex(dest_mac.replace(':', ''))
         eth_header = struct.pack('!6s6sH', dst_mac_bytes, src_mac_bytes, eth_type)
-        ethpacket = eth_header + packet
-        eths.sendto(ethpacket, (interface, 0))
-        
+        eth_packet = eth_header + packet
+        print(f"Eth Packet: {eth_packet}")
+        eths.sendto(eth_packet, (interface, 0))
+
 def verify_port(port):
-    return 0 <= port <= 65535
+    if 0 <= port <= 65535:
+        return True, ""
+    else:
+        return False, f"{port} is not a valid port number."
 
 def verify_ip(ip):
     parts = ip.split('.')
-    if len(parts) != 4:
-        return False
-    for part in parts:
-        if not part.isdigit() or not 0 <= int(part) <= 255:
-            return False
-    return True  
-    
+    if len(parts) == 4 and all(part.isdigit() and 0 <= int(part) <= 255 for part in parts):
+        return True, ""
+    else:
+        return False, f"{ip} is not a valid IP address."
+
 def verify_mac(mac):
-    pattern = re.compile(
-        r"^"
-        r"([0-9A-Fa-f]{2}[:-]){5}"
-        r"([0-9A-Fa-f]{2})"
-        r"$"
-    )
+    pattern = re.compile(r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$")
     if pattern.match(mac):
-        return True
+        return True, ""
     else:
-        return False
-    
-def get_user_input():
-    spoof = input("Do you want to spoof the IP source address? (y/n): ")
-    if spoof.lower() == 'y':
-        source_ip = input("Enter the IP address to spoof: ")
-        if verify_ip(source_ip):
-            print(f"{source_ip} is a valid IP address. Spoofing enabled.")
+        return False, f"{mac} is not a valid MAC address."
+
+def handle_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dev', type=str, help='Spoofing mode - SrcMac@SrcIp:SrcPort>DstMac@DstIp:DstPort')
+    parser.add_argument('-dst', type=str, help='Packet destinaton - Mac@Ip:Port (Port is optional)')
+#    parser.add_argument('-e', type=str, help='Add encryption password (Optional)')
+#    password = e
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-Ds', type=str, default='Welcome to DotSlashProtocol!', help='Data string')
+    group.add_argument('-Df', type=str, help='Data file')
+    args = parser.parse_args()
+    if args.dev:
+        src, dst = args.dev.split('>')
+        srcmac, srcip_srcport = src.split('@')
+        srcip, srcport = srcip_srcport.split(':')
+        dstmac, dstip_dstport = dst.split('@')
+        dstip, dstport = dstip_dstport.split(':')
+    elif args.dst:
+        dstport = 80
+        srcport = 80
+        dstmac, dstip_dstport = args.dst.split('@')
+        if ':' in dstip_dstport:
+            dstip, dstport = dstip_dstport.split(':')
         else:
-            print(f"{source_ip} is not a valid IP address. Please enter a valid IP address.")
+            dstip = dstip_dstport
+        srcip = socket.gethostbyname(socket.gethostname())
+        mac_num = hex(uuid.getnode()).replace('0x', '')
+        srcmac = ':'.join(mac_num[i: i + 2] for i in range(0, 11, 2))
+        
     else:
-        source_ip = socket.gethostbyname(socket.gethostname())
-    spoofm = input("Do you want to spoof the MAC source address? (y/n): ")
-    if spoofm.lower() == 'y':
-        mac = input("Enter the MAC address to spoof: ")
-        if verify_mac(mac):
-            print(f"{mac} is a valid MAC address. Spoofing enabled.")
-        else:
-            mac = 'c0:53:1c:c0:53:1c'
-    else:
-        mac = 'c0:53:1c:c0:53:1c'
-    dest_mac = input("Enter the destination MAC address (default: c0:53:1c:c0:53:1c):") or 'c0:53:1c:c0:53:1c'
-    if verify_mac(dest_mac):
-        print(f"{dest_mac} is a valid MAC address.")
-    else:
-        dest_mac = 'c0:53:1c:c0:53:1c'
-    dest_ip = input("Enter the destination IP address (default: 192.168.1.2):") or '192.168.1.2'
-    if not verify_ip(dest_ip):
-        print(f"{dest_ip} is not a valid IP address.")
-    source_port = int(input("Enter the source port (default: 80):") or 80)
-    if not verify_port(source_port):
-        print(f"{source_port} is not a valid port number.")
-    dest_port = int(input("Enter the destination port (default: 80):") or 80)
-    if not verify_port(dest_port):
-        print(f"{dest_port} is not a valid port number.")
-    
-    print(f"DSP Source IP Address: {source_ip}:{source_port}\nDSP Source MAC Address: {mac}\nDSP Destination IP Address: {dest_ip}:{dest_port}\nDSP Destination MAC Address: {dest_mac}")
-    data_type = input("Do you want to enter data, upload a file, or default: (data/file/default)")
-    if data_type.lower() == "data":
-        data = input("Enter the data:")
-    elif data_type.lower() == "file":
-        filename = input("Enter the filename:")
-        try:
-            with open(filename, 'r') as file:
-                data = file.read()
-        except FileNotFoundError:
-            print("File not found. Using default data.")
-            data = 'Welcome to DotSlashProtocol!'
+        print("Either -dev or -dst argument is required.")
+        return
+    if args.Ds and args.Ds != 'True':
+        data = args.Ds
+    elif args.Df:
+        with open(args.Df, 'r') as file:
+            data = file.read()
     else:
         data = 'Welcome to DotSlashProtocol!'
-    return source_ip, dest_ip, source_port, dest_port, dest_mac, data
+#    return srcip, dstip, srcport, dstport, srcmac, dstmac, data, password
+    return srcip, dstip, srcport, dstport, srcmac, dstmac, data
 
 if __name__ == "__main__":
     print("Welcome to DotSlashProtocol - A TCP/IP Fork")
-    source_ip, dest_ip, source_port, dest_port, dest_mac, data = get_user_input()
-    send = DSP(source_ip, dest_ip, source_port, dest_port, dest_mac, data)
+#    srcip, dstip, srcport, dstport, srcmac, dstmac, data, password = handle_args()
+#    send = DSP(srcip, dstip, srcport, dstport, srcmac, dstmac, data, password)
+    srcip, dstip, srcport, dstport, srcmac, dstmac, data = handle_args()
+    send = DSP(srcip, dstip, srcport, dstport, srcmac, dstmac, data)
     send.dsp()
